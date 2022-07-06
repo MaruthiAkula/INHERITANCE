4942023727e7a0ff51e803119821cc643a9b391b
maruthi.akula <maruthi.akula@PUN-LAP-DEV105>
2022-07-06 09:40:41 +0530

    Add project files.

---
+++ INHERITANCE/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INHERITANCE
+{
+    public class Baseclass
+    {
+      public string studentname;
+      public string fathername;
+      public string number;
+    }
+    public class Derivedclass:Baseclass
+    {
+       public string fathernum;
+       public string studentnum;
+      /*  void Numfunction()
+        {
+            Console.WriteLine("father Num is {0},Student Num is {1}", fathernum, studentnum);
+        }*/
+    }
+    public class childclass:Derivedclass
+    {
+       public string fatheroccupation;
+       public string studentqualification;
+        public void printalldetails()
+        {
+            Console.WriteLine("father Name is {0},Student Name is {1}", fathername, studentname);
+            Console.WriteLine("father occupation is {0},Student qualification is {1}", fatheroccupation, studentqualification);
+            Console.WriteLine("father Num is {0},Student Num is {1}", fathernum, studentnum);
+
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            childclass obj = new childclass();
+            obj.studentnum = "rahul";
+            obj.fathername = "sitaramaraju";
+            obj.fathernum = "99999999999";
+            obj.studentnum = "11111111111";
+            obj.studentqualification = "B.tech";
+            obj.fatheroccupation = "farmer";
+            obj.printalldetails();
+        }
+    }
+}
---
+++ INHERITANCE/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("INHERITANCE")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("INHERITANCE")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9972c89c-cace-4777-b278-d44ee2bd3757")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
